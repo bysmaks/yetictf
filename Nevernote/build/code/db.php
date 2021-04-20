625e5945407cd79f8a6aefdf4847d9854945f06f
Maks <by.sm@protonmail.com>
2021-04-20 11:35:34 +0700

    fix

--- Nevernote/build/code/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once('helper.php');
-
-$db = ConnectDatabase();
-$stmt = $db->prepare("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, username TEXT, password TEXT, uuid TEXT)");
-$stmt->execute();
-?>
